286ed29af25d4092d3e9eebd5b2a04361d5bf390
PaulCabreaEPN <paul.cabrera@epn.edu.ec>
2024-08-09 16:34:10 -0500

    se agrega el carrito

--- src/componentes/menu.js
+++ src/componentes/menu.js
@@ -2,6 +2,7 @@ import React from 'react';
 import "./menu.css";
 import { useNavigate } from 'react-router-dom';
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import carrito from './imagenes/Iconos/carrito.png'
 
 export const Menu = () => {
 
@@ -10,7 +11,13 @@ export const Menu = () => {
         <Navbar className="navbar-custom" variant="dark" expand="lg">
         
         <Navbar.Brand href="#home">COFFEE & DREAMS</Navbar.Brand>
-      
+        <Navbar.Brand href="#home" id='carrito'><a 
+              className="ml-3" 
+              href="#" 
+            >
+              <img src={carrito} alt="Usuario" width="40" />
+        </a></Navbar.Brand>
+
     </Navbar>
     </div>
     
